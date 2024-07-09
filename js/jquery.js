608aa4eb66c1c83fb38ec1376b3f967efa6239cc
Sainjargal Bolortsetseg <azure.blr24@gmail.com>
2024-07-09 18:39:18 +0900

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,4 @@
+/*$(document).on('scroll',function(){
+	$('.moving-txt-1').css("left",Math.max(-500 + 0.35*window.scrollY)+"px");
+	$('.moving-txt-2').css("right",Math.max(-500 + 0.35*window.scrollY)+"px");
+})*/
