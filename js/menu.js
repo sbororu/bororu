853f2431cae96fe58f2ea05abe761f51fa8b7594
Sainjargal Bolortsetseg <azure.blr24@gmail.com>
2024-07-11 16:52:04 +0900

    Delete js directory

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-/*Hamburger menu*/
-function menuOnClick(){
-	document.getElementById("menu_checkbox").classList.toggle("change");
-	
-	document.getElementById("nav").classList.toggle("change");
-	document.getElementById("menu-bg").classList.toggle("change-bg");
-}
